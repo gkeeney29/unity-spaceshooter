dc04f652022eda97af41cd00164268f415e45cee
Greg Keeney <gkeeney29@gmail.com>
2015-09-18 14:57:29 -0400

    shooter

---
+++ Space Shooter/Assets/Scripts/RandomRotater.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotater : MonoBehaviour 
+{
+	public float tumble;
+
+	void Start()
+	{
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+
+}
